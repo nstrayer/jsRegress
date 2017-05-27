41ba27e5bc463563916dcd7bab3647deac9b3cd3
Nick Strayer <nick@nicks-mbp.attlocal.net>
2017-05-27 17:41:32 -0500

    added a node on paused manual inversion work

--- src/invertLowerTri.js
+++ src/invertLowerTri.js
@@ -1,2 +1,13 @@
 // function to invert a lower triangular matrix. This will be used in conjunction
 // with the cholesky function to do matrix inversion of any matrix.
+
+//Paused dev here. If I end up still needing this I will come back to it though.
+const invertLowerTri = (L) => {
+
+}
+
+const L = [
+  [ 5, 0, 0],
+  [ 3, 3, 0],
+  [-1, 1, 3]
+];
